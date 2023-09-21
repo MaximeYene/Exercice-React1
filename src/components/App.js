6c4e3b9a682bba3b8f47f37c3b3e1f2241eb05eb
Maxime Yene <maximeyene@gmail.com>
2023-09-21 21:37:25 +0100

    Exercise React 1

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import '../App.css';
+import Presentation from './PresentationPlayer';
+
+function App() {
+  return (<Presentation/>)
+}
+
+export default App;
---
+++ src/components/UserList.js
@@ -0,0 +1,38 @@
+export const user = [
+    {
+        id : "00a",
+        Nom: "alain",
+        Prenom: "stephane",
+        Age: 28,
+        Emploi: "enseignant",
+        Hobby: "basket",
+        Descritption: "lunettes"
+    },
+    {
+        id : "01b",
+        Nom: "cyrielle",
+        Prenom: "brigite",
+        Age: 25,
+        Emploi: "Etudiante",
+        Hobby: "tennis",
+        Descritption: "lenettes"
+    },
+    {
+        id : "02c",
+        Nom: "mireille",
+        Prenom: "julie",
+        Age: 23,
+        Emploi: "Etudiante",
+        Hobby: "sport",
+        Descritption: "mince"
+    },
+    {
+        id : "03d",
+        Nom : "Kalhed",
+        Prenom : "Moustafa",
+        Age : "16",
+        Emploi : "Eleve",
+        Hobby : "Football",
+        Descritption : "Petit et mince"
+    }
+]
